cdb81243203c40be94b52140fa69847013b6365e
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-05-22 15:41:45 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Configuración centralizada de la API
 export const API_CONFIG = {
-  BASE_URL: "http://192.168.27.27:8080/api",
+  BASE_URL: "http://192.168.240.27:8080/api",
+  BASE_URL_WS: "ws://192.168.240.27:8080"
 };
