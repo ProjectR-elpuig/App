248a920e8951c2b0ae0b8242a30cf2e76137f14c
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-05-30 21:55:52 +0200

    dist

--- dist/assets/keyboard2-legacy-jj_4kDhN.js
+++ dist/assets/keyboard2-legacy-jj_4kDhN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C3dQ_SF6.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DcvM5ULj.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- dist/assets/md.transition-legacy-9S4mvpQG.js
+++ dist/assets/md.transition-legacy-9S4mvpQG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C3dQ_SF6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DcvM5ULj.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
