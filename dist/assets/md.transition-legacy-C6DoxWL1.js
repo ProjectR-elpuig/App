de5454e351ae27a36830126a72d62b072ea23027
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-05-30 21:32:08 +0200

    dist

--- dist/assets/md.transition-legacy-C6DoxWL1.js
+++ dist/assets/md.transition-legacy-C6DoxWL1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CuNMH7xn.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-C3dQ_SF6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
